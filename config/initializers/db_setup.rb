9824f08494be7c43e2d216a6c83384c5ce82bc62
bugloper <bugloper@gmail.com>
2024-10-13 13:01:20 +0600

    change app name

--- config/initializers/db_setup.rb
+++
@@ -1,8 +0,0 @@
-if Rails.env.production?
-  begin
-    ActiveRecord::Base.connection
-  rescue ActiveRecord::NoDatabaseError
-    puts "Creating database..."
-    system("bundle exec rails db:create db:migrate")
-  end
-end
