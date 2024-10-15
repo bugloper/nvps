17d1a639eb8773d8497a74f037388e31c5d82568
bugloper <bugloper@gmail.com>
2024-10-15 20:41:41 +0600

    add DO ips

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,4 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
-  before_action :setup_db
-
-  private
-
-  def setup_db
-   puts "Creating database..."
-   system("bundle exec rails db:create db:migrate")
-  rescue StandardError
-    system("bundle exec rails db:migrate")
-  end
 end
